b8ddcf31f6d1616275ab84dfab69a27d577f5d3d
Swider <andy.frot@outlook.fr>
2022-06-27 11:23:38 +0200

    Nouveau Modèle Utilisateur

---
+++ class/User.php
@@ -0,0 +1,60 @@
+<?php
+class User {
+    private $id;
+    private $username;
+    private $email;
+    private $password;
+    private $profile_image;
+    private $back_image;
+    private $description;
+    private $notes;
+    private $comments;
+    private $last_connection;
+
+
+    function __construct($id) {
+        $this->id = $id;
+    }
+
+
+    function postComment($comment) {
+        //
+    }
+    function modifyComment($id, $comment) {
+        //
+    }
+    function eraseComment($id) {
+        //
+    }
+    function postNote($note) {
+        //
+    }
+    function modifyNote($id, $note) {
+        //
+    }
+    function eraseNote($id) {
+        //
+    }
+
+    function getId() { return $this->id; }
+    function setId($id) { $this->id = $id; }
+    function getUsername() { return $this->username; }
+    function setUsername($username) { $this->username = $username; }
+    function getEmail() { return $this->email; }
+    function setEmail($email) { $this->email = $email; }
+    function getPassword() { return $this->password; }
+    function setPassword($password) {
+        $this->password = $password;
+    }
+    function getProfileImage() { return $this->profile_image; }
+    function setProfileImage($profile_image) { $this->profile_image = $profile_image; }
+    function getBackImage() { return $this->back_image; }
+    function setBackImage($back_image) { $this->back_image = $back_image; }
+    function getDescription() { return $this->description; }
+    function setDescrption($description) { $this->description = $description; }
+    function getNotes() { return $this->notes; }
+    function getComments() { return $this->comments; }
+    function getLastConnection() { return $this->last_connection; }
+    function setLastConnection($last_connection) { $this->last_connection = $last_connection; }
+}
+?>
