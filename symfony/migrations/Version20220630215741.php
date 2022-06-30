32f76fb4e3dddf3a33e08db86ffd2a23235b0f09
RicardFabien <49694912+RicardFabien@users.noreply.github.com>
2022-07-01 00:00:11 +0200

    fix: migration would fail

--- symfony/migrations/Version20220630215741.php
+++ symfony/migrations/Version20220630215741.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220630130257 extends AbstractMigration
+final class Version20220630215741 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20220630130257 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE animanga CHANGE genres genres VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE animanga ADD genres VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE animanga CHANGE genres genres LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'(DC2Type:array)\'');
+        $this->addSql('ALTER TABLE animanga DROP genres');
     }
 }
